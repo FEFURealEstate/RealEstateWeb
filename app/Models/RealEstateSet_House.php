8820bd25150395d4e371bcfef7fdae96b3761754
Denis <dennis220802@gmail.com>
2022-01-12 21:07:37 +1000

    add column 'total_rooms' in RealEstateSet_House

--- app/Models/RealEstateSet_House.php
+++ app/Models/RealEstateSet_House.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
  * @property int $id
  * @property float|null $total_area
  * @property int|null $total_floors
+ * @property int|null $total_rooms
  * @property-read \App\Models\RealEstateSet $realEstate
  * @method static \Illuminate\Database\Eloquent\Builder|RealEstateSet_House newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|RealEstateSet_House newQuery()
@@ -17,6 +18,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
  * @method static \Illuminate\Database\Eloquent\Builder|RealEstateSet_House whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RealEstateSet_House whereTotalArea($value)
  * @method static \Illuminate\Database\Eloquent\Builder|RealEstateSet_House whereTotalFloors($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RealEstateSet_House whereTotalRooms($value)
  */
 class RealEstateSet_House extends BaseModel
 {
